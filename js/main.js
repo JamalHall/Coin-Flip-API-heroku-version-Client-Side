e049943e79b3498c5c68ca3b1995ce605778da5e
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-09 09:40:21 -0500

    counter added

--- js/main.js
+++ js/main.js
@@ -24,6 +24,23 @@ async function flip(){
         let objectURL = data.imageUrl
         document.getElementById('image').setAttribute("src",objectURL)
         document.getElementById('save').innerText= res+" "+document.getElementById('save').innerText
+        counter(data.msg)
     }
     catch(err) {console.log(err)}
+    }
+
+    function counter(res){
+        document.getElementById('total').innerText = tot
+        document.getElementById('heads').innerText = hed
+        document.getElementById('tails').innerText = tai
+        let tot=0
+        let hed=0
+        let tai=0
+        if(res=='heads'){
+            total+=1
+            heads+=1
+        } else{
+            total+=1
+            tai+=1
+        }
     }
