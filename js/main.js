a7a02299494b7f35cca98842c4b4b54b25cc1d34
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-09 09:57:51 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,12 +18,16 @@ async function flip(){
     try{
         const response = await fetch('https://coinflip-fullstack.herokuapp.com/coin/')
         const data = await response.json()
+
         console.log(data.msg)
         let res=data.msg.toUpperCase()
+
         console.log(data.imageUrl)        
         let objectURL = data.imageUrl
+        
         document.getElementById('image').setAttribute("src",objectURL)
         document.getElementById('save').innerText= res+" "+document.getElementById('save').innerText
+
         counter(data.msg)
     }
     catch(err) {console.log(err)}
@@ -32,9 +36,8 @@ async function flip(){
         let tot =0
         let hed =0
         let tai =0
-    function counter(res){
-        console.log(res)
-        
+
+    function counter(res){         
         document.getElementById('total').innerText = tot
         document.getElementById('heads').innerText = hed
         document.getElementById('tails').innerText = tai
